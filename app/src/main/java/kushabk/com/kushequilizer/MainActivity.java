b2e50b437b712cbce2642187096152e8fe35bbb7
KUSHA B K <kusha.b.k@gmail.com>
2016-03-16 09:27:16 +0530

    code cleanup

--- app/src/main/java/kushabk/com/kushequilizer/MainActivity.java
+++ app/src/main/java/kushabk/com/kushequilizer/MainActivity.java
@@ -97,7 +97,7 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
 
         updateUserInterface();
         // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        
         client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
