274b6c129e848b4638ed8916c282406c710c07c6
KUSHA B K <kusha.b.k@gmail.com>
2016-04-07 23:28:04 +0530

    Update ApplicationTest.java

--- app/src/androidTest/java/kushabk/com/kushequilizer/ApplicationTest.java
+++ app/src/androidTest/java/kushabk/com/kushequilizer/ApplicationTest.java
@@ -5,6 +5,7 @@ import android.test.ApplicationTestCase;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ * kusha b k
  */
 public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
