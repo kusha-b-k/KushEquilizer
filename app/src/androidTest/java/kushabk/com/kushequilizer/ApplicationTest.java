35f036fd195101b2d019acb71f9bb17b63aa4c62
KUSHA B K <kusha.b.k@gmail.com>
2016-07-15 23:08:57 +0530

    cleanup

--- app/src/androidTest/java/kushabk/com/kushequilizer/ApplicationTest.java
+++ app/src/androidTest/java/kushabk/com/kushequilizer/ApplicationTest.java
@@ -3,10 +3,7 @@ package kushabk.com.kushequilizer;
 import android.app.Application;
 import android.test.ApplicationTestCase;
 
-/**
- * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
- * kusha b k
- */
+
 public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
         super(Application.class);
